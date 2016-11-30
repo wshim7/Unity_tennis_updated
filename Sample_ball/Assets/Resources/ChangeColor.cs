1c3f99e3c9f873b753af7c889214a8aa6e95c1db
wshim7 <wgshim7@gmail.com>
2016-11-30 11:14:20 -0500

    updated

---
+++ Sample_ball/Assets/Resources/ChangeColor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+    Renderer render;
+    private float t = 0;
+    private float duration = 1;
+	// Use this for initialization
+	void Start () {
+        render = GetComponent<Renderer> ();
+	}
+    void Update()
+    {
+
+        if (transform.position.y < 3.7 && transform.position.y >3)
+        {
+            render.material.color = Color.Lerp(Color.green,Color.red,t);
+            if (t < 1)
+            {
+                t += Time.deltaTime / duration;
+            }
+        }
+        if (transform.position.y < 1.5 && transform.position.y > 1.39)
+        {
+            t = 0;
+            render.material.color = Color.Lerp(Color.red, Color.green,t);
+            if (t < 1)
+            {
+                t += Time.deltaTime / duration;
+            }
+        } 
+    }
+    // Update is called once per frame
+    
+    //void OnCollisionEnter(Collision col)
+    //{
+        /*if (col.gameObject.tag == "court")
+        {
+            render.material.color = Color.Lerp(Color.red, Color.green, 0);
+            print(transform.lossyScale.y);
+        }
+        */
+
+    //}
+   
+}
---
+++ Sample_ball/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClick : MonoBehaviour {
+
+	// Use this for initialization
+	public void clickT()
+    {
+        if (GameObject.FindWithTag("ball").transform.position.y < 3.7 && GameObject.FindWithTag("ball").transform.position.y > 0) 
+        {
+            print("success");
+            print(GameObject.FindWithTag("ball").transform.position.y);
+            return;
+        }
+        print("fail");
+        print(GameObject.FindWithTag("ball").transform.position.y);
+        return;
+    }
+}
